95eb085f9deb53d3157c437cc4404c62d6f5fcb9
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2026-03-28 19:41:17 +0100

    small checker refactor

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -28,13 +28,13 @@ public class Checker {
 
         // pre-population of available functions and duplicate function check
         for (ASTNode node : ast.root.getChildren()) {
-            if (node instanceof FunctionDefinition functionDefinition) {
-                String functionName = functionDefinition.name;
-                if (availableFunctions.containsKey(functionName)) {
-                    functionDefinition.setError("Function '" + functionName + "' is already defined");
-                } else {
-                    availableFunctions.put(functionName, functionDefinition);
-                }
+            if (!(node instanceof FunctionDefinition functionDefinition)) continue;
+
+            String functionName = functionDefinition.name;
+            if (availableFunctions.containsKey(functionName)) {
+                functionDefinition.setError("Function '" + functionName + "' is already defined");
+            } else {
+                availableFunctions.put(functionName, functionDefinition);
             }
         }
 
@@ -43,129 +43,29 @@ public class Checker {
 
     // checkNode is called recursively, so it's possible for the node to be any type of node in the AST
     private void checkNode(ASTNode node) {
-        // nodes that create a new scope: StyleRule, Stylesheet, IfClause, ElseClause
-        if (node instanceof StyleRule || node instanceof Stylesheet || node instanceof IfClause || node instanceof ElseClause) {
+        // boolean for if this node creates a new scope
+        boolean createsNewScope = node instanceof StyleRule || node instanceof Stylesheet || node instanceof IfClause || node instanceof ElseClause;
+
+        if (createsNewScope) {
             // new scope, so push new hashmap to list
             variableTypes.addFirst(new HashMap<>());
         }
 
-        // variable assignment
-        if (node instanceof VariableAssignment variableAssignment) {
-            String name = variableAssignment.name.name;
-            ExpressionType varType = getExpressionType(variableAssignment.expression);
-
-            // check if variable is already defined in current scope, and if it's the same type
-            boolean found = false;
-            for (HashMap<String, ExpressionType> scope : variableTypes) {
-                if (scope.containsKey(name)) {
-                    found = true;
-                    ExpressionType existingType = scope.get(name);
-                    if (existingType != varType) {
-                        // set error if types don't match
-                        variableAssignment.setError("Variable '" + name + "' can't be redefined with a different type. Existing type: " + existingType + ", new type: " + varType);
-                    }
-                    // don't need to update the value, since this is the checker, not the evaluator. just break loop
-                    break;
-                }
-            }
-
-            if (!found) {
-                // if not found, add it to the current scope (which is the first hashmap in the list)
-                variableTypes.getFirst().put(name, varType);
-            }
-        }
-
-        // CH01 + CH06: variables should be defined, and only used within their scope
-        else if (node instanceof VariableReference variableReference) {
-            String name = variableReference.name;
-            // search through the variable name in the symbol table, starting from current scope and going up
-            boolean found = false;
-            for (HashMap<String, ExpressionType> scope : variableTypes) {
-                if (scope.containsKey(name)) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                // if not found, set error on the variable reference node
-                variableReference.setError("CH01/CH06: Variable '" + name + "' is undefined");
-            }
-        }
-
-        // CH02: check operands of PLUS/MIN for either the exact same types, or scalar+other
-        else if (node instanceof AddOperation || node instanceof SubtractOperation) {
-            ExpressionType typeofLhs = getExpressionType(((Operation) node).lhs);
-            ExpressionType typeofRhs = getExpressionType(((Operation) node).rhs);
-
-            // skip if one of the sides already has an error
-            if (typeofLhs == null || typeofRhs == null) return;
-
-            if (typeofLhs != typeofRhs && typeofLhs != ExpressionType.SCALAR && typeofRhs != ExpressionType.SCALAR) {
-                node.setError("CH02: Incompatible add/subtract types: " + typeofLhs + " and " + typeofRhs);
-            }
-        }
-        // CH02: check operands of MUL for at least one scaler
-        else if (node instanceof MultiplyOperation) {
-            ExpressionType typeofLhs = getExpressionType(((Operation) node).lhs);
-            ExpressionType typeofRhs = getExpressionType(((Operation) node).rhs);
-
-            if (typeofLhs == null || typeofRhs == null) return;
-
-            if (typeofLhs != ExpressionType.SCALAR && typeofRhs != ExpressionType.SCALAR) {
-                node.setError("CH02: Incompatible multiplication types: " + typeofLhs + " and " + typeofRhs + ". At least one operand of a multiplication must be a scalar.");
-            }
-        }
-
-        // CH04: check if a property's value is of the correct type
-        else if (node instanceof Declaration declaration) {
-            String propertyName = declaration.property.name;
-
-            // propertyName must be one of color, background-color, width, height
-            if (!List.of("color", "background-color", "width", "height").contains(propertyName)) {
-                declaration.setError("Unknown property '" + propertyName + "'. Allowed properties are: color, background-color, width, height.");
+        switch (node) {
+            case VariableAssignment variableAssignment -> validateVariableAssignment(variableAssignment);
+            case VariableReference variableReference -> validateVariableReference(variableReference);
+            case AddOperation ignored -> validateCalculationOperation(node);
+            case SubtractOperation ignored2 -> validateCalculationOperation(node);
+            case MultiplyOperation ignored3 -> validateCalculationOperation(node);
+            case Declaration declaration -> validateDeclaration(declaration);
+            case IfClause ifClause -> validateIfClause(ifClause);
+            case FunctionDefinition ignored -> {
+                // explicitly skip function definitions, because they are checked when called.
+                // skipping prevents the checkNode from being called on its children,
+                //   which otherwise will cause errors of undefined variables (it doesn't recognize params as params)
                 return;
             }
-
-            ExpressionType valueType = getExpressionType(declaration.expression);
-
-            // if valueType is null, skip because there is another error
-            if (valueType == null) return;
-
-            // if valueType is SCALAR, it's never correct because it needs to be a specific unit
-            if (valueType == ExpressionType.SCALAR) {
-                declaration.setError("CH04: Property '" + propertyName + "' cannot be of type SCALAR");
-                return;
-            }
-
-            // color/background-color = COLOR (or variable with that type)
-            if ((propertyName.equals("color") || propertyName.equals("background-color"))
-                    && valueType != ExpressionType.COLOR) {
-                declaration.setError("CH04: Property '" + propertyName + "' must be of type COLOR. Type is: " + valueType);
-            }
-            // width/height = PIXEL/PERCENTAGE (or variable with one of those types)
-            else if ((propertyName.equals("width") || propertyName.equals("height"))
-                    && valueType != ExpressionType.PIXEL && valueType != ExpressionType.PERCENTAGE) {
-                declaration.setError("CH04: Property '" + propertyName + "' must be of type PIXEL or PERCENTAGE. Type is: " + valueType);
-            }
-        }
-
-        // CH05: check if if-clause is boolean
-        else if (node instanceof IfClause ifClause) {
-            ExpressionType conditionType = getExpressionType(ifClause.conditionalExpression);
-
-            // skip if existing error
-            if (conditionType == null) return;
-
-            if (conditionType != ExpressionType.BOOL) {
-                ifClause.setError("CH05: If-condition must be of type BOOL. Type is: " + conditionType);
-            }
-        }
-
-        // skip function definitions, because they are checked when called.
-        else if (node instanceof FunctionDefinition) {
-            // skipping also prevents the checkNode from being called on its children,
-            //   which otherwise will cause errors of undefined variables (it doesn't recognize params as params)
-            return;
+            default -> {}
         }
 
         // check all children of this node for errors
@@ -174,9 +74,111 @@ public class Checker {
         }
 
         // nodes with ending scope
-        if (node instanceof StyleRule || node instanceof Stylesheet || node instanceof IfClause || node instanceof ElseClause) {
+        if (createsNewScope) {
             // end of scope, so "pop" the hashmap from the list
             variableTypes.removeFirst();
+        }
+    }
+
+    private void validateVariableAssignment(VariableAssignment variableAssignment) {
+        String name = variableAssignment.name.name;
+        ExpressionType varType = getExpressionType(variableAssignment.expression);
+
+        // check if variable is already defined in current scope, and if it's the same type
+        for (HashMap<String, ExpressionType> scope : variableTypes) {
+            if (scope.containsKey(name)) {
+                ExpressionType existingType = scope.get(name);
+                if (existingType != varType) {
+                    // set error if types don't match
+                    variableAssignment.setError("Variable '" + name + "' can't be redefined with a different type. Existing type: " + existingType + ", new type: " + varType);
+                }
+                // don't need to update the value, since this is the checker, not the evaluator. just break loop
+                return;
+            }
+        }
+
+        // if not found, add it to the current scope (which is the first hashmap in the list)
+        variableTypes.getFirst().put(name, varType);
+    }
+
+    // CH01 + CH06: variables should be defined, and only used within their scope
+    private void validateVariableReference(VariableReference variableReference) {
+        String name = variableReference.name;
+
+        // search through the variable name in the symbol table, starting from current scope and going up
+        for (HashMap<String, ExpressionType> scope : variableTypes) {
+            if (scope.containsKey(name)) {
+                return;
+            }
+        }
+
+        // if not found, set error on the variable reference node
+        variableReference.setError("CH01/CH06: Variable '" + name + "' is undefined");
+    }
+
+    private void validateCalculationOperation(ASTNode node) {
+        ExpressionType typeofLhs = getExpressionType(((Operation) node).lhs);
+        ExpressionType typeofRhs = getExpressionType(((Operation) node).rhs);
+
+        boolean isAddOrSubtract = node instanceof AddOperation || node instanceof SubtractOperation;
+        boolean isMultiply = node instanceof MultiplyOperation;
+
+        // skip if one of the sides already has an error
+        if (typeofLhs == null || typeofRhs == null) return;
+
+        // CH02: check operands of PLUS/MIN for either the exact same types, or scalar+other
+        if (isAddOrSubtract && typeofLhs != typeofRhs && typeofLhs != ExpressionType.SCALAR && typeofRhs != ExpressionType.SCALAR) {
+            node.setError("CH02: Incompatible add/subtract types: " + typeofLhs + " and " + typeofRhs);
+        }
+
+        // CH02: check operands of MUL for at least one scaler
+        if (isMultiply && typeofLhs != ExpressionType.SCALAR && typeofRhs != ExpressionType.SCALAR) {
+            node.setError("CH02: Incompatible multiplication types: " + typeofLhs + " and " + typeofRhs + ". At least one operand of a multiplication must be a scalar.");
+        }
+    }
+
+    // CH04: check if a property's value is of the correct type
+    private void validateDeclaration(Declaration declaration) {
+        String propertyName = declaration.property.name;
+
+        // propertyName must be one of color, background-color, width, height
+        if (!List.of("color", "background-color", "width", "height").contains(propertyName)) {
+            declaration.setError("Unknown property '" + propertyName + "'. Allowed properties are: color, background-color, width, height.");
+            return;
+        }
+
+        ExpressionType valueType = getExpressionType(declaration.expression);
+
+        // if valueType is null, skip because there is another error
+        if (valueType == null) return;
+
+        // if valueType is SCALAR, it's never correct because it needs to be a specific unit
+        if (valueType == ExpressionType.SCALAR) {
+            declaration.setError("CH04: Property '" + propertyName + "' cannot be of type SCALAR");
+            return;
+        }
+
+        // color/background-color = COLOR (or variable with that type)
+        if ((propertyName.equals("color") || propertyName.equals("background-color"))
+                && valueType != ExpressionType.COLOR) {
+            declaration.setError("CH04: Property '" + propertyName + "' must be of type COLOR. Type is: " + valueType);
+        }
+        // width/height = PIXEL/PERCENTAGE (or variable with one of those types)
+        else if ((propertyName.equals("width") || propertyName.equals("height"))
+                && valueType != ExpressionType.PIXEL && valueType != ExpressionType.PERCENTAGE) {
+            declaration.setError("CH04: Property '" + propertyName + "' must be of type PIXEL or PERCENTAGE. Type is: " + valueType);
+        }
+    }
+
+    // CH05: check if if-clause is boolean
+    private void validateIfClause(IfClause ifClause) {
+        ExpressionType conditionType = getExpressionType(ifClause.conditionalExpression);
+
+        // skip if existing error
+        if (conditionType == null) return;
+
+        if (conditionType != ExpressionType.BOOL) {
+            ifClause.setError("CH05: If-condition must be of type BOOL. Type is: " + conditionType);
         }
     }
 
